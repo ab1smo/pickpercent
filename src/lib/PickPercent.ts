80de8b79465a3adebae6b2c1f8d651437888b193
Abi <ab1smo@users.noreply.github.com>
2022-05-31 00:13:08 +0400

    refactor: use lodash

--- src/lib/PickPercent.ts
+++ src/lib/PickPercent.ts
@@ -1,40 +1,37 @@
+import _ from 'lodash';
 import { IAddItem } from './interfaces/IAddItem';
 
 export class PickPercent {
-  private array: IAddItem[] = [];
+  private items: IAddItem[] = [];
 
-  public add(options: IAddItem) {
-    const once = this.array.filter((item) => item.once);
-
-    if (once.includes(options)) {
-      throw TypeError(
-        `Item "${options}" was already added with option "once: true"`
-      );
+  public add(option: IAddItem) {
+    if (option.percent < 0 || option.percent > 100) {
+      throw TypeError(`Item "${option.name}" - percentage out of range!`);
     }
 
-    if (options.percent < 0 || options.percent > 100) {
-      throw TypeError(`Item "${options}" - percentage out of range!`);
-    }
+    this.items.push(option);
+  }
 
-    this.array.push(options);
+  public addMany(options: IAddItem[]) {
+    for (const option of options) {
+      this.add(option);
+    }
   }
 
   public final() {
-    if (this.array.length === 0) return null;
+    if (this.items.length === 0) return null;
 
-    const total = this.array.reduce<number>((acc, item) => {
-      acc += item.percent;
-      return acc;
-    }, 0);
+    let random = Math.random() * 100;
 
-    const random = Math.random() * total;
+    for (const item of this.items) {
+      random -= item.percent;
 
-    const result = this.array
-      .sort((a, b) => a.percent - b.percent)
-      .find((item) => random <= item.percent);
-    console.log(random, result);
-
-    if (result?.once) this.array.slice(this.array.indexOf(result));
-    if (result) return result.name;
+      if (random < 0) {
+        if (item?.repeats && --item.repeats <= 0) {
+          _.remove(this.items, item);
+        }
+        return item.name;
+      }
+    }
   }
 }
